52b52e09552ca9a2c729791af232fa6de8c6992d
Hanjeong Lee <gkswjdzz@naver.com>
2020-01-28 14:24:02 +0900

    edit link of mask_file to solve zip error in server.js

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var express = require("express"),
   cors = require("cors"),
   Busboy = require("busboy"),
   fs = require("fs"),
-  uuidv4 = require('uuid/v4');
-const { spawn } = require('child_process');
+  uuidv4 = require('uuid/v4'),
+  path = require('path')
+const { spawn } = require('child_process')
 
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use(cors({
   origin: 'https://ainize.ai',
-}));
+}))
 
-var repo_dir = '.';
+var repo_dir = '.'
 
 const model_names = ['craft_mlt_25k', 'craft_ic15_20k', 'craft_refiner_CTW1500']
 const params = ['zip', 'text', 'image']
@@ -20,40 +21,39 @@ const params = ['zip', 'text', 'image']
 //return [filename uploaded, model_name]
 function busboyFunc(req, res) {
   return new Promise((resolve, reject) => {
-    let fileuploaded = true;
-    let model = true;
+    let fileuploaded = true
     let temp = new Map()
-    var busboy = new Busboy({ headers: req.headers });
-    uuid4 = uuidv4();
-    busboy.on("file", function(fieldname, file, filename, encoding, mimetype) {
+    var busboy = new Busboy({ headers: req.headers })
+    uuid4 = uuidv4()
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
       if (filename === "") {
-        fileuploaded = false;
+        fileuploaded = false
       }
-      file.pipe(fs.createWriteStream(__dirname + '/data/' + uuid4 + '.jpg'));
-    });
+      file.pipe(fs.createWriteStream(__dirname + '/data/' + uuid4 + '.jpg'))
+    })
 
-    busboy.on("field", function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    busboy.on("field", (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) =>
       temp.set(fieldname, val)
-    });
+    )
 
-    busboy.on("finish", function() {
+    busboy.on("finish", () => {
       if (!fileuploaded) {
-        res.writeHead(400);
-        res.end();
-        return;
+        res.writeHead(400)
+        res.end()
+        return
       }
-      console.log('busboy function resolve');
-      resolve([uuid4, temp.get('model')]);
-    });
+      console.log('busboy function resolve')
+      resolve([uuid4, temp.get('model')])
+    })
     
-    req.pipe(busboy);
-  }).then(function(data){
+    req.pipe(busboy)
+  }).then(data => {
     console.log('uuid is ' + data[0] + ' model is ' + data[1])
     return [__dirname + '/data/' + data[0] + '.jpg', data[1]]
   })
 }
 
-app.post('/textdetect/:format', async (req, res) => {
+app.post('/:format', async (req, res) => {
   format = req.params.format
   console.log(format)
   
@@ -63,7 +63,7 @@ app.post('/textdetect/:format', async (req, res) => {
     return
   }
 
-  const [inputname, model] = await busboyFunc(req, res);
+  const [inputname, model] = await busboyFunc(req, res)
   
   console.log(model)
   if(!(model_names.includes(model))){
@@ -72,8 +72,8 @@ app.post('/textdetect/:format', async (req, res) => {
       res.end()
       return
   }
+  
   //[--trained_model, --text_threshold, --low_text, --link_threshold, --cuda, --canvas_size, --mag_ratio, --poly, --show_time, --test-folder, --refine, --refiner_model]
-  //configs = [model + '.pth', 0.7, 0.4, 0.4, 'False', 1280, 1.5, 'False', 'False', __dirname + '/data/', 'False', 'weights/craft_refiner_CTW1500.pth']
   configs = [model + '.pth', 0.7, 0.4, 0.4, 'False', 1280, 1.5, 'False', 'False', inputname, 'False', 'weights/craft_refiner_CTW1500.pth']
 
   code = await runPython(configs)
@@ -85,50 +85,86 @@ app.post('/textdetect/:format', async (req, res) => {
     res.end()
     return
   }
-  filename = inputname.split('.')[0]
 
+  dirname = path.dirname(inputname)
+  split = inputname.split('/')
+  filename = dirname + '/res_' + split[split.length - 1]
+  filename = filename.split('.')[0]
   maskOutput = filename + '_mask.jpg'
   txtOutput = filename + '.txt'
   jpgOutput =  filename + '.jpg'
 
-  if(format != 'zip'){
+  if(format == 'zip'){
+    var zip = spawn('zip', ['-rj', '-', maskOutput, txtOutput, jpgOutput])
+      // Keep writing stdout to res
+    res.contentType('zip')
+    zip.stdout.on('data', data => res.write(data))
+    zip.stderr.on('data', data => console.log('zip stderr: ' + data))
+
+    // End the response on zip exit
+    zip.on('exit', code => {
+      if(code !== 0) res.statusCode = 500
+      console.log('zip process exited with code ' + code)
+      res.end()
+    })
+  }else if(format == 'text'){
+    let txt = spawn('cat', [txtOutput])
+    let result = ''
+      // Keep writing stdout to res
+    res.contentType('json')
+    
+    txt.stdout.on('data', data => result += data)
+    txt.stderr.on('data', data => 
+      console.log('txt stderr: ' + data)
+    )
+
+    // End the response on txt exit
+    txt.on('exit', (code) => {
+      if(code !== 0) {
+        res.statusCode = 500
+        console.log('zip process exited with code ' + code)
+        res.end()
+      } else {
+        quadrangles = result.split('\r\n')
+        
+        let ret = {}
+        for(let i = 0; i < quadrangles.length; i++){
+          let quadrangle = quadrangles[i].split(',')
+          temp = {}
+          if(quadrangle[0] == '')
+            break
+          temp['lu'] = [quadrangle[0], quadrangle[1]]
+          temp['ru'] = [quadrangle[2], quadrangle[3]]
+          temp['rd'] = [quadrangle[4], quadrangle[5]]
+          temp['ld'] = [quadrangle[6], quadrangle[7]]
+          ret[i] = temp
+        }
+        console.log(ret)
+        res.json(ret)
+      }
+    })
+  }else if(format =='image'){
+    var img = fs.createReadStream(jpgOutput)
+    img.on('open', () => {
+      res.set('Content-Type', 'image/png')
+      img.pipe(res)
+    })
+    img.on('exit', (code) =>
+      console.log('image spawn exit : ' + code)
+    )
+  }else{
     console.log('sorry ! support only zip format')
     res.end()
     return
   }
-
-  var zip = spawn('zip', ['-rj', '-', maskOutput, txtOutput, jpgOutput]);
-    // Keep writing stdout to res
-  res.contentType('zip');
-  
-  zip.stdout.on('data', function (data) {
-    res.write(data);
-  });
-  
-  zip.stderr.on('data', function (data) {
-    // Uncomment to see the files being added
-    console.log('zip stderr: ' + data);
-  });
-
-  // End the response on zip exit
-  zip.on('exit', function (code) {
-    if(code !== 0) {
-        res.statusCode = 500;
-        console.log('zip process exited with code ' + code);
-        res.end();
-    } else {
-        res.end();
-    }
-  });
 })
 app.listen(80, () => {
-  console.log("server connect");
-});
+  console.log("server connect")
+})
 
 //run python except densepose
 runPython = (configs) => {
   return new Promise((resolve, reject) => {
-    let ret = '';
     let conf = [
       repo_dir + "/test.py",
       '--trained_model', configs[0],
@@ -144,13 +180,14 @@ runPython = (configs) => {
       //'--refine', configs[10], 
       //'--refiner_model', configs[11], 
     ]
-    const pyProg = spawn('python', conf);
+    const pyProg = spawn('python', conf)
     pyProg.stderr.on('data', (data) => {
       console.log(data.toString())
       resolve(data.toString())
     })
-    pyProg.on('exit', (code) =>
+    pyProg.on('exit', (code) =>{
+      console.log('pyProg exit code : ' + code)
       resolve(code)
-    )
+    })
   })
-};
+}
